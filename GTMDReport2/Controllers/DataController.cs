200df89ab9052737ce96dd2d1579d5a4b7c83fb3
tianmaoyu <tianmaoyu@foxmail.com>
2016-11-23 19:58:05 +0800

    加入概况

---
+++ GTMDReport2/Controllers/DataController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GTMDReport2.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Summary
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Summary/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Summary/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Summary/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Summary/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Summary/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Summary/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Summary/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GTMDReport2/Controllers/SummaryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GTMDReport2.Controllers
+{
+    public class SummaryController : Controller
+    {
+        // GET: Summary
+        public ActionResult Index()
+        {
+            string viewName = "Index" + Request.QueryString["year"];
+            return View(viewName);
+        }
+    }
+}
