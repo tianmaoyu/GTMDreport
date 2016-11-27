66e829645ff3d9d473ec534993d3d2dbd87d2e85
tianmaoyu <tianmaoyu@foxmail.com>
2016-11-27 12:12:36 +0800

    把贵州地图文件改成api 形式

---
+++ GTMDReport2/API/JsonDataController.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace GTMDReport2.API
+{
+    public class JsonDataController : ApiController
+    {
+        [HttpGet]
+        public JObject GuiZhou()
+        {
+            string path = HttpContext.Current.Server.MapPath("~/JsonData/guizhou.json");
+            using (StreamReader file = File.OpenText(path))
+            {
+                string guizhou = file.ReadToEnd();
+                return JObject.Parse(guizhou);
+            }
+        }
+    }
+}
